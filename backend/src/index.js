e8cc943930952fe9523783f5050205d67d27fb76
Arthur Meireles <arthur_meireles@hotmail.com.br>
2020-08-26 10:24:10 -0300

    Iniciando Backend

---
+++ backend/src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { uuid } = require('uuidv4');
+
+const app = express();
+app.use(express.json());
+
+const projects = [];
+
+app.get('/projects', (req, res) => {
+	return res.json(projects);
+});
+
+app.post('/projects', (req, res) => {
+	const { title, owner } = req.body;
+	const project = { id: uuid(), title, owner };
+	projects.push(project);
+
+	return res.json(project);
+});
+
+app.put('/projects/:id', (req, res) => {
+	const { id } = req.params;
+
+	const projectIndex = projects.findIndex(project => project.id === id);
+
+	if (projectIndex < 0) {
+		return res.status(404).json({ error: 'Project not found' });
+	}
+
+	return res.json();
+});
+
+app.delete('/projects/:id', (req, res) => {
+	const { id } = req.params;
+
+	return res.json(['projeto 2', 'projeto 3']);
+});
+
+app.listen(3333, () => {
+	console.log('🚀 Back-end started!');
+});

9c23cfa5e13dbd4b15f9316dcfd0e6347abb823a
Arthur Meireles <arthur_meireles@hotmail.com.br>
2020-08-27 15:31:14 -0300

    FInalizando ambiente react js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
-const soma = (a, b) => {
-	return a + b;
-};
+import React from 'react';
+import { render } from 'react-dom';
 
-console.log(soma(2, 2));
+
+render(<h1>Hello World</h1>, document.getElementById('app')); 

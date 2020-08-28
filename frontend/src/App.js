ee474dc4948b343449521dacfd7c09e3ed2f5b55
Arthur Meireles <arthur_meireles@hotmail.com.br>
2020-08-28 13:17:00 -0300

    Consumindo Backend com Axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import api from './services/api';
 import './App.css';
 
 function App() {
-    const [projects, setProjects] = useState(['Web Dev', 'Web Production']);
-    
-    useEffect(() => {
-        api.get('/projects').then(res => {
-            console.log(res)
-        })
-    }, []);
+	const [projects, setProjects] = useState([]);
+
+	useEffect(() => {
+		api.get('projects').then(res => {
+			setProjects(res.data);
+		});
+	}, []);
 
 	//useState retorna array com 2 posições [state, setState]
 	// 1. A variavel com o valor inicial
@@ -28,7 +28,7 @@ function App() {
 			<Header title="Projects" />
 			<ul>
 				{projects.map(project => (
-					<li key={project}>{project}</li>
+					<li key={project.id}>{project.title}</li>
 				))}
 			</ul>
 			<button type="button" onClick={handleAddProject}>

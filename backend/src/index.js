98ada8c0419959e9118498638a8474737b08ae60
Arthur Meireles <arthur_meireles@hotmail.com.br>
2020-08-28 12:52:48 -0300

    Configurando Cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const { uuid, isUuid } = require('uuidv4');
 const cors = require('cors');
 
-app.use(cors());
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 const projects = [];

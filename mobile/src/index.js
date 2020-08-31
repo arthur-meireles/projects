1c33ee3edd6524e155fa411d5a75da192f051979
Arthur Meireles <arthur_meireles@hotmail.com.br>
2020-08-31 12:39:18 -0300

    Buscando dados da API e exibindo em FlatList

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import api from './services/api';
 
 export default function App() {
+	const [projects, setProjects] = useState([]);
+
+	useEffect(() => {
+		api.get('projects').then(res => {
+			console.log(res.data);
+			setProjects(res.data);
+		});
+	}, []);
+
 	return (
 		<>
-			<StatusBar barStyle='light-content'/>
-			<View style={styles.container}>
-				<Text style={styles.title}>Hello GoStack</Text>
-			</View>
+			<StatusBar barStyle="light-content" />
+            <SafeAreaView style={styles.container}>
+			<FlatList
+				data={projects}
+				keyExtractor={project => project.id}
+				renderItem={({ item: project }) => (
+					<Text style={styles.project}>{project.title}</Text>
+				)}
+			/>
+            </SafeAreaView>
 		</>
 	);
 }
@@ -16,12 +32,11 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#7159c1',
-		justifyContent: 'center',
-		alignItems: 'center',
 	},
-	title: {
+	project: {
 		color: '#FFF',
 		fontSize: 32,
 		fontWeight: 'bold',
+		margin: 8,
 	},
 });

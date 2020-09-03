63dd5316fba58fd91e421be74f9f03e9a0b60d10
Arthur Meireles <arthur_meireles@hotmail.com.br>
2020-09-03 12:03:44 -0300

    Tipando variaveis e vetores

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,23 @@
 import { Request, Response, response } from 'express';
+import createUser from './services/CreateUser';
+
+// string, number, boolean, object, Array
+// interfaces
 
 export function helloWorld(request: Request, response: Response) {
+	const user = createUser({
+		email: 'arthurmeireles11@gmail.com',
+		password: '123123',
+		techs: [
+			'Node.js',
+			'React',
+			'React Native',
+			{ title: 'javascript', experience: 100 },
+		],
+		hobbies: ['Correr', 'Jogar aquele CS'],
+	});
+
+	console.log(user.name);
+
 	return response.json({ message: 'Hello World' });
 }
